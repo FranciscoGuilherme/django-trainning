c5f184d676f43db3237114783880f8f461b76a96
Francisco Guilherme <franciscoguilherme.mundo@outlook.com>
2022-05-07 22:30:16 -0400

    feat(rota): implementa GET simples.

--- crudapp/views.py
+++ crudapp/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import get_list_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from . models import BusinessRule
+from . serializers import BusinessRuleSerializer
+
+class BusinessList(APIView):
+    def get(self, request):
+        businessRule = BusinessRule.objects.all()
+        serializer = BusinessRuleSerializer(businessRule, many = True)
+
+        return Response(serializer.data)
+
+    def post(self):
+        pass
