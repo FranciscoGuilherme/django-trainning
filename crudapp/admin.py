141520a96bcdea8c077feee99d09b4fad8e55e80
Francisco Guilherme <franciscoguilherme.mundo@outlook.com>
2022-05-10 21:49:37 -0400

    refactor(projeto): remove modulo nao utilizado.

--- crudapp/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from . models import BusinessRule
-
-admin.site.register(BusinessRule)
-# Register your models here.
--- crudapp/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from . models import BusinessRule
-
-class BusinessRuleSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = BusinessRule
-        fields = ('tags', 'information')
